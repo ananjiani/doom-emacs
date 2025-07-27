c2bd0c9810c27d735c0325c0dc6e9f1518eb584d
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-07-26 20:41:27 -0500

    remove elisp mcp

--- packages.el
+++ packages.el
@@ -53,7 +53,7 @@
 (package! org-roam-ui)
 (package! git-auto-commit-mode)
 (package! org-download)
-(package! elisp-dev-mcp)
+;; (package! elisp-dev-mcp)
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
